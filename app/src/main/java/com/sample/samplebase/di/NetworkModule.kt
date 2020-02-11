e24bce1b87dc605ab209d5df125ca74c58622fbc
tran.van.canh <tran.van.canh@sun-asterisk.com>
2020-02-11 10:17:13 +0700

    Config base di

---
+++ app/src/main/java/com/sample/samplebase/di/NetworkModule.kt
@@ -0,0 +1,5 @@
+package com.sample.samplebase.di
+
+import org.koin.dsl.module
+
+val networkModule = module {} //TODO
---
+++ app/src/main/java/com/sample/samplebase/di/RepositoryModule.kt
@@ -0,0 +1,5 @@
+package com.sample.samplebase.di
+
+import org.koin.dsl.module
+
+val repositoryModule = module {} //TODO
